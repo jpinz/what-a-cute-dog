aa0fd4b8105eb0c92c8652cf921fe60f491f7e12
Julian <julian.pinzer@gmail.com>
2021-07-29 19:22:41 -0400

    Initial commit with actual web code

---
+++ components/Tutorial.vue
@@ -0,0 +1,19 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Come find a cute dog!
+        </h2>
+        <p class="mt-3 text-gray-600">
+          We recommend you take a look at the <a href="https://nuxtjs.org" target="_blank" class="text-green-500 hover:underline">Nuxt documentation</a>, whether you are new or have previous experience with the framework.<br>
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+          To get started, remove <code class="bg-gray-100 text-sm p-1 rounded border">components/Tutorial.vue</code> and start coding in <code class="bg-gray-100 text-sm p-1 rounded border">pages/index.vue</code>. Have fun!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
