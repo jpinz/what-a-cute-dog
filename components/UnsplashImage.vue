04f208a8aec3b1b3949763e6cc6c49dd4be9c18c
Julian <julian.pinzer@gmail.com>
2021-07-30 00:34:14 -0400

    Added backup dog api lol

--- components/UnsplashImage.vue
+++ components/UnsplashImage.vue
@@ -3,19 +3,23 @@
     <p v-if="$fetchState.pending">Fetching doggos...</p>
     <p v-else-if="$fetchState.error">An error occurred :(</p>
     <div v-else>
-      <img :src="picture.urls.raw" />
-      <p>{{picture.description}}</p>
-      <br/>
-      <p><a :href="picture.user.links.html">{{picture.user.name}}</a> - from <a href="unsplash.com">unsplash.com</a></p>
+      <img :src="pictureUrl" />
+      <p>{{ description }}</p>
+      <br />
+      <p v-if="picture.urls != null">
+        <a :href="picture.user.links.html">{{ picture.user.name }}</a> - from
+        <a href="unsplash.com">unsplash.com</a>
+      </p>
       <button class="btn btn-blue" @click="$fetch">Refresh</button>
     </div>
   </div>
 </template>
 <script>
-
-function handleErrors(response) {
+async function handleErrors(response) {
   if (!response.ok) {
-    throw Error(response.statusText);
+    return await fetch("https://dog.ceo/api/breeds/image/random").then(
+      (res) => res
+    );
   }
   return response;
 }
@@ -25,6 +29,22 @@ export default {
     return {
       picture: {},
     };
+  },
+  computed: {
+    pictureUrl: function () {
+      if (this.picture.urls) {
+        return this.picture.urls.raw;
+      } else {
+        return this.picture.message;
+      }
+    },
+    description: function () {
+      if (this.picture.description) {
+        return this.picture.description;
+      } else {
+        return "A cute dog!";
+      }
+    },
   },
   async fetch() {
     this.picture = await fetch(
